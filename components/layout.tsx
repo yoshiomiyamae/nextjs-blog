88f33ea4ebe92b2792d92f74fedc77cf19eaf2b1
Yoshio Miyamae <yoshio.miyamae@famoce-succellion.com>
2022-01-08 16:55:09 +0900

    first commit

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,10 +4,15 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
+const name = 'Yoshio Miyamae'
 export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
+interface LayoutProps {
+  children: React.ReactNode
+  home?: boolean
+}
+
+export default function Layout({ children, home }: LayoutProps) {
   return (
     <div className={styles.container}>
       <Head>
